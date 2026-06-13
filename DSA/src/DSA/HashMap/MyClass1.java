9eeeffacc87b3edc4b73da6955070430f8b56427
11ranjitreddy <150681516+11ranjitreddy@users.noreply.github.com>
2026-06-13 11:20:22 +0530

    hasmap user input

--- DSA/src/DSA/HashMap/MyClass1.java
+++ DSA/src/DSA/HashMap/MyClass1.java
@@ -1,4 +1,18 @@
 package DSA.HashMap;
+import  java.util.*;
 
 public class MyClass1 {
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        HashMap<Integer,Integer> map=new HashMap<>();
+        int n=s.nextInt();
+        for(int i=0;i<n;i++){
+            System.out.print("Enter the key:");
+            int key=s.nextInt();
+            System.out.print("Enter the value:");
+            int value=s.nextInt();
+            map.put(key,value);
+        }
+        System.out.print(map);
+    }
 }
