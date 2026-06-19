f3a482628343b6568b19ca2cb0a729a799808779
11ranjitreddy <150681516+11ranjitreddy@users.noreply.github.com>
2026-06-19 12:01:32 +0530

    Encapsulation 1

---
+++ DSA/src/DSA/Oops2/BankAccountEncapsulation.java
@@ -0,0 +1,37 @@
+package DSA.Oops2;
+
+import  java.util.*;
+public class BankAccountEncapsulation {
+    public static  void  main(String args[]){
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter the Balance");
+        int b=s.nextInt();
+        Bank bank=new Bank();
+        bank.setBalance(b);
+        System.out.println(bank.getBalance());
+        System.out.println("Enter the Amount to Deposit");
+        int amount=s.nextInt();
+        bank.deposit(amount);
+        System.out.println("Current Balance: "+bank.getBalance());
+        System.out.println("Enter the Amount to withdraw: ");
+        int with=s.nextInt();
+        bank.withdraw(with);
+        System.out.println("Remaining balance"+bank.getBalance());
+    }
+}
+
+class Bank{
+    private int balance;
+
+    public int getBalance(){return balance;}
+    public void setBalance(int balance){this.balance=balance;}
+
+    int deposit(int amount){
+       return balance+=amount;
+    }
+    int withdraw(int amount){
+        if(amount<=balance)
+            balance-=amount;
+    return balance;
+    }
+}
