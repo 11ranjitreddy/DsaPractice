9650c70aeb6fd3a797c248573a3263c8007ce793
11ranjitreddy <150681516+11ranjitreddy@users.noreply.github.com>
2026-06-20 16:08:39 +0530

    123

--- DSA/src/DSA/EncodingAndDecoding/ConvertWordToNumber.java
+++ DSA/src/DSA/EncodingAndDecoding/ConvertWordToNumber.java
@@ -2,6 +2,9 @@ package DSA.EncodingAndDecoding;
 
 public class ConvertWordToNumber {
     public static void main(String args[]){
-
+      String str="HELLO";
+      for(char ch:str.toCharArray()){
+          System.out.println((ch-'A')+1);
+      }
     }
 }
---
+++ DSA/src/DSA/EncodingAndDecoding/EncodeLettersToLetters.java
@@ -0,0 +1,11 @@
+package DSA.EncodingAndDecoding;
+
+public class EncodeLettersToLetters {
+    public static void main(String args[]){
+        for(int i=0;i<26;i++){
+            char ch=(char)(i+97);
+
+            System.out.println(ch);
+        }
+    }
+}
